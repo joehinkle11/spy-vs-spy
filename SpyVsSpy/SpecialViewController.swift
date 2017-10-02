8b491a8e425f5c71ea8dc4dca7677dadd036112f
Joseph Hinkle <josephhinkle@MacBooks-MacBook-Pro.local>
2017-10-02 14:30:09 -0400

    more view changes

---
+++ SpyVsSpy/SpecialViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpyVsSpy
+//
+//  Created by Joseph Hinkle on 9/19/17.
+//  Copyright © 2017 Joseph Hinkle. All rights reserved.
+//
+
+import UIKit
+
+class SpecialViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
