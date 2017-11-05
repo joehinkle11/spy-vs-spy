5bb0211d56e59fe7ddc75b534e82430bc2bebbc3
Candace <crallison96@gmail.com>
2017-11-05 00:25:25 -0400

    Timer begins on button press, progress bar gives user idea of where timer is at.

--- SpyVsSpy/HackViewController.swift
+++ SpyVsSpy/HackViewController.swift
@@ -16,13 +16,30 @@ class HackViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var currentLongitude: UILabel!
     @IBOutlet weak var currentLocation: UILabel!
     @IBOutlet weak var hackbtn: UIButton!
+    @IBOutlet weak var progressbar: UIProgressView!
+    @IBOutlet weak var hackcomplete: UILabel!
+    @IBOutlet weak var hack_img: UIImageView!
     
     //Set up CLLlocation instance
     var manager = CLLocationManager()
     
+    //Set timer
+    var seconds = 60
+    var time: Float = 0
+    
+    //Create timer
+    var timer = Timer()
+    
+    //Create image
+    let checkImage = UIImage(named: "checkmark")
+    let xImage = UIImage(named: "redX")
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        //Hide image
+        hack_img.isHidden = true
         
         //Draw circular button
         hackbtn.layer.cornerRadius = 0.5 * hackbtn.bounds.size.width
@@ -30,7 +47,13 @@ class HackViewController: UIViewController, CLLocationManagerDelegate {
         hackbtn.backgroundColor = UIColor.red
         hackbtn.setTitle("HACK", for: .normal)
         hackbtn.setTitleColor(UIColor.white, for: .normal)
-        view.addSubview(hackbtn)
+        //hackbtn.isHidden = true
+        
+        //Hide progress bar
+        progressbar.isHidden = true
+        
+        //Hide label
+        hackcomplete.isHidden = true
         
         //Set up location manager
         manager.delegate = self
@@ -102,6 +125,19 @@ class HackViewController: UIViewController, CLLocationManagerDelegate {
             }
         }
         
+        if (hack_img.image == xImage) //Red X - Not in range/Already hacked
+        {
+            hack_img.isHidden = true
+            hackbtn.isHidden = false
+            hackcomplete.isHidden = true
+        }
+        else if(hack_img.image == checkImage) //Just hacked
+        {
+            hackbtn.isHidden = true
+            hack_img.isHidden = false
+            hackcomplete.isHidden = true
+        }
+        
         //Rename label
         currentLocation.text = name
     }
@@ -109,5 +145,58 @@ class HackViewController: UIViewController, CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion)
     {
         currentLocation.text = "Out of Range"
+        hack_img.image = xImage
+        hack_img.isHidden = false
+        hackbtn.isHidden = true
+    }
+    
+    @IBAction func start_hacking(_ sender: Any)
+    {
+        //Enable progress bar
+        progressbar.isHidden = false
+        hackcomplete.isHidden = true
+        
+        print("Button clicked")
+        
+        //Decrement timer
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(HackViewController.updateTimer), userInfo: nil, repeats: true)
+    }
+ 
+    func updateTimer()
+    {
+        //Time
+        time = time + 0.01667
+        
+        //Set progress
+        progressbar.setProgress(Float(time), animated: true)
+        
+        seconds -= 1
+        print(seconds)
+        
+        if (seconds == 0)
+        {
+            //Stop timer
+            timer.invalidate()
+            print("Timer Complete")
+            
+            //Set progress bar to 100%
+            progressbar.setProgress(1.0, animated: true)
+            
+            //Hide progress bar and unhide label
+            progressbar.isHidden = true
+            hackcomplete.isHidden = false
+            
+            //Change button to image
+            hackbtn.isHidden = true
+            hack_img.image = checkImage
+            hack_img.isHidden = false
+            
+            //Reset timer value
+            seconds = 60
+            time = 0
+            
+            //Reset progress bar
+            progressbar.setProgress(0.0, animated: true)
+        }
     }
 }
