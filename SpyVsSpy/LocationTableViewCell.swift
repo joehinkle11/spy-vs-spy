37c7ada38a6ad0f9af39b8d4a034758f51e1964c
Candace <crallison96@gmail.com>
2017-11-24 02:05:00 -0500

    Added segment in home screen to toggle between looking at active players and buildings to hack. Added list of buildings to hack - list updates as buildings are hacked.

---
+++ SpyVsSpy/LocationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationTableViewCell.swift
+//  SpyVsSpy
+//
+//  Created by Candicane on 11/23/17.
+//  Copyright © 2017 Joseph Hinkle. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
